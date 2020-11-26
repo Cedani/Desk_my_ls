f528217fec061a1bf445d2032458749315c0deae
Cedani <aboh-daniel-cedrick.kakou@epitech.eu>
2020-11-26 16:08:51 +0100

    adding(master): adding information, and code in my_ls_l function

--- main.c
+++ main.c
@@ -34,12 +34,23 @@ void write_time(struct stat stat1)
 
 void my_ls_l(char *name, struct stat stat1)
 {
-
+    write_type_file(stat1);
+    write_permission(stat1);
+    write(1, " ", 1);
+    my_put_nbr(stat1.st_nlink);
+    write(1, " ", 1);
+    write_username(stat1);
+    write(1, " ", 1);
+    my_put_nbr(stat1.st_size);
+    write(1, " ", 1);
+    write_time(stat1);
+    write(1, " ", 1);
+    write(1, name, strlen(name));
 }
 
 void loop(char *filepath)
 {
-
+    // on appellera la fonction my_ls_l dans cette fonction
 }
 
 int main(int argc, char **argv)
